0ae4b17b9af00878af23e3413158fbf1a988fd84
Angebriela <angelachiyal07@gmail.com>
2025-06-09 08:37:39 -0600

    Agregar archivos de proyecto.

---
+++ Models/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace FINALPROGRA3.Models
+{
+    public class Animal
+    {
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Especie { get; set; }
+        [Required] 
+        public int Edad { get; set; }
+    }
+}
